2cee3a46a8f5fb41d79bdc8506cd41df79982b06
Mathias Rav <rav@cs.au.dk>
2017-04-03 21:26:24 -0400

    Add new main function

--- smb-frames.py
+++ smb-frames.py
@@ -249,5 +249,20 @@ def main():
         json.dump(obj, fp, indent=4)
 
 
+def darbian_splits():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input-filename', '-i', required=True)
+    parser.add_argument('--crop', '-c', required=True, type=crop_string)
+    parser.add_argument('--crop-labels', '-l', required=True, type=crop_string)
+    args = parser.parse_args()
+
+    framerate, all_frames = extract_frames(args.input_filename, args.crop)
+    framerate, all_splits = extract_frames(args.input_filename, args.crop_labels)
+    nframes, height, width, channels = all_frames.shape
+
+    for i, (f1, f2, extra) in enumerate(find_levels(framerate, all_frames)):
+        print("Detected split at %s" % f2)
+
+
 if __name__ == '__main__':
-    main()
+    darbian_splits()
